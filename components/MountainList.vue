43181cf243a04a8974557cb1a3d9793288b105de
hyojeongyun <ynhyojng@gmail.com>
2022-06-29 13:06:08 +0900

    about에 component 추가 + index에 css

--- components/MountainList.vue
+++ components/MountainList.vue
@@ -3,14 +3,17 @@ const { data: mountains } = await useAsyncData("mountains", () => $fetch("https:
 </script>
 <template>
     <div>
-        <h2>Mountains</h2>
-        <ul>
-            <li v-for="mountain in mountains" :key="mountain.title">
-                <NuxtLink :to="'/mountains/' + mountain.slug">
-                    {{ mountain.title }}
-                </NuxtLink>
-            </li>
-        </ul>
+        <div class="list">
+            <h2>Mountains</h2>
+            <ul>
+                <li v-for="mountain in mountains" :key="mountain.title">
+                    <NuxtLink :to="'/mountains/' + mountain.slug">
+                        {{ mountain.title }}
+                    </NuxtLink>
+                </li>
+            </ul>
+            <br />
+        </div>
         <br />
     </div>
 </template>
@@ -18,5 +21,16 @@ const { data: mountains } = await useAsyncData("mountains", () => $fetch("https:
 <style scoped>
 ul {
     display: contents;
+    text-align: center;
+}
+.list {
+    text-align: center;
+    width: 300px;
+    border: 1px solid green;
+    left: 50%;
+}
+
+div {
+    margin: 0px auto;
 }
 </style>
--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+const posts = [
+    { id: 1, title: "my journey" },
+    { id: 2, title: "nuxt" },
+];
+const count = ref(0);
+</script>
+
 <template>
     <div>
         <img src="~/assets/logo.svg" />
         <h1>About</h1>
+        <div v-for="post in posts" :key="post.title">
+            <p>{{ post.title }}</p>
+        </div>
+        <div>
+            <span>{{ count }}</span>
+            <br />
+            <button @click="count++">count 증가</button>
+            <button @click="count--">count 감소</button>
+        </div>
     </div>
 </template>
 
@@ -11,8 +29,5 @@ img {
 }
 h1 {
     text-align: center;
-}
-ul {
-    display: contents;
 }
 </style>
